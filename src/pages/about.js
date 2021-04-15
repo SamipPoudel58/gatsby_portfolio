a62f7d8fb0018dfeb64a0b8da60c9174041d7465
Samip Poudel <sawmeep58@gmail.com>
2021-04-15 21:10:02 +0545

    created layer wrapper component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,19 +1,24 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo et
-        eligendi animi facilis laudantium voluptate quos blanditiis atque, at
-        consequuntur ex minima iusto delectus cum illum totam adipisci quidem?{" "}
-      </p>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo et
-        eligendi animi facilis laudantium voluptate quos blanditiis atque, at
-        consequuntur ex minima iusto delectus cum illum totam adipisci quidem?{" "}
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo
+          et eligendi animi facilis laudantium voluptate quos blanditiis atque,
+          at consequuntur ex minima iusto delectus cum illum totam adipisci
+          quidem?{" "}
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo
+          et eligendi animi facilis laudantium voluptate quos blanditiis atque,
+          at consequuntur ex minima iusto delectus cum illum totam adipisci
+          quidem?{" "}
+        </p>
+      </div>
+    </Layout>
   )
 }

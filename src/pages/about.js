3804ee5fd03289a397144d15edc12959145423ee
Samip Poudel <sawmeep58@gmail.com>
2021-04-15 20:45:39 +0545

    created basic pages

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo et
+        eligendi animi facilis laudantium voluptate quos blanditiis atque, at
+        consequuntur ex minima iusto delectus cum illum totam adipisci quidem?{" "}
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nemo et
+        eligendi animi facilis laudantium voluptate quos blanditiis atque, at
+        consequuntur ex minima iusto delectus cum illum totam adipisci quidem?{" "}
+      </p>
+    </div>
+  )
+}

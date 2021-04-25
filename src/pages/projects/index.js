61140f7a69fd3149078fbd5f7b3f80e14509683a
Samip Poudel <sawmeep58@gmail.com>
2021-04-25 18:26:34 +0545

    created and used styles

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import { portfolio } from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>Projects</div>
+      <div className={portfolio}>
+        <h1>Projects</h1>
+        <h3>Projects & Websites I've Created</h3>
+      </div>
     </Layout>
   )
 }

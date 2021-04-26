b97c2700189581686a6b97c630e870ece7947f6e
Samip Poudel <sawmeep58@gmail.com>
2021-04-26 19:26:20 +0545

    added gatsby-transform-remark package and more markdown files

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,41 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/projects.module.css"
+import { projects, portfolio } from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  const myprojects = data.allMarkdownRemark.nodes
   return (
     <Layout>
       <div className={portfolio}>
         <h1>Projects</h1>
         <h3>Projects & Websites I've Created</h3>
+        <div className={projects}>
+          {myprojects.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
